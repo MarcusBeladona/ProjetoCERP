7e3d983ec3c23946d0a80685fc9353a98a00ad62
larah <larahvirginia@alu.ufc.br>
2022-06-15 17:13:14 -0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./util/styles/global.css";
 import "./util/styles/typos.css";
 import "./util/styles/colors.css";
 
-//import { LoginPage } from "../src/components/pages/LoginPage";
+// import { LoginPage } from "../src/components/pages/LoginPage";
 import "./App.css";
 import React from "react";
 import { PaginaInicial } from "./components/pages/PaginaInicial/PaginaInicial";
