28928b8714c0afba0f159f78922912ffafabe7da
larah <larahvirginia@alu.ufc.br>
2022-06-26 18:04:51 -0300

    coloquei navigate nas coisas - larah

--- client/src/components/commons/TabelaAvaliacao/TabelaAvaliacao.js
+++ client/src/components/commons/TabelaAvaliacao/TabelaAvaliacao.js
@@ -1,91 +1,83 @@
-import "./TabelaAvaliacao.css"
-import React from "react"
+import "./TabelaAvaliacao.css";
+import React from "react";
 import { Button } from "reactstrap";
+import { useNavigate } from "react-router-dom";
 
+export function TabelaAvaliacao() {
+  const navigate = useNavigate();
 
-export function TabelaAvaliacao(){
-    
-    return(
+  return (
     <div>
-        <table className="tabela">
-            <thead>
-                <tr>
-                <th>
-                    
-                </th>
-                <th>
-                    Avaliador
-                </th>
-                <th>
-                    Data de Avaliação
-                </th>
-                <th>
-                    Tipo de Avaliação
-                </th>
+      <table className="tabela">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Avaliador</th>
+            <th>Data de Avaliação</th>
+            <th>Tipo de Avaliação</th>
 
-                <th>
-                    Ações
-                </th>
-                </tr>
+            <th>Ações</th>
+          </tr>
+        </thead>
 
+        <tbody className="body-tabela">
+          <tr className="inicio-tabela">
+            <th scope="row">1</th>
+            <td>Hermes</td>
+            <td>03/07/2020</td>
+            <td className="objetivo">
+              <div className="card-objetivo">Antropométrica</div>
+            </td>
 
-            </thead>
+            <td className="botoes">
+              <Button
+                outline
+                color="primary"
+                className="botao"
+                onClick={() => {
+                  navigate("/visualizarAvaliacao");
+                }}
+              >
+                Visualizar
+              </Button>
+              <Button
+                outline
+                color="warning"
+                className="botao"
+                onClick={() => {
+                  navigate("/editarAvaliacao");
+                }}
+              >
+                Editar
+              </Button>
+              <Button color="danger" className="botao">
+                Excluir
+              </Button>
+            </td>
+          </tr>
 
+          <tr className="inicio-tabela">
+            <th scope="row">1</th>
+            <td>Hermes</td>
+            <td>03/07/2020</td>
+            <td className="objetivo">
+              <div className="card-objetivo">Par-q</div>
+            </td>
 
-
-            <tbody className="body-tabela">
-                <tr className="inicio-tabela">
-                <th scope="row">
-                    1
-                </th>
-                <td>
-                    Hermes
-                </td>
-                <td>
-                    03/07/2020
-                </td>
-                <td className="objetivo">
-                    <div className="card-objetivo">
-                        Antropométrica   
-                    </div>
-                </td>
-                
-                <td className="botoes">
-                    <Button outline color="primary" className="botao">Visualizar</Button>
-                    <Button outline color="warning" className="botao">Editar</Button>
-                    <Button color="danger" className="botao">Excluir</Button>
-
-                </td>
-                </tr>
-
-                <tr className="inicio-tabela">
-                <th scope="row">
-                    1
-                </th>
-                <td>
-                    Hermes
-                </td>
-                <td>
-                    03/07/2020
-                </td>
-                <td className="objetivo">
-                    <div className="card-objetivo">
-                        Par-q   
-                    </div>
-                </td>
-
-                <td className="botoes">
-                    <Button outline color="primary" className="botao">Visualizar</Button>
-                    <Button outline color="warning" className="botao">Editar</Button>
-                    <Button color="danger" className="botao">Excluir</Button>
-
-                </td>
-                </tr>
-                
-                                           
-            </tbody>
-        </table>
+            <td className="botoes">
+              <Button outline color="primary" className="botao">
+                Visualizar
+              </Button>
+              <Button outline color="warning" className="botao">
+                Editar
+              </Button>
+              <Button color="danger" className="botao">
+                Excluir
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-
-    )
+  );
 }
