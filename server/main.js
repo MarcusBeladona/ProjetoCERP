962c4d25e8cfa95bba5d497c3069fcf04b984e93
Marcus Beladona <mv3265@gmail.com>
2022-07-10 20:24:56 -0300

    testee

--- server/main.js
+++ server/main.js
@@ -2,12 +2,9 @@ const app = require("./config/express");
 const db = require("./config/mongoose");
 
 // Express
-app.listen(3010, function () {
+app.listen(process.env.PORT || 3010, function () {
 	console.log("Express online: localhost:3010");
 });
 
 // MongoDB
-db.setup(
-	"mongodb+srv://projetoCERP:Utw8ufjB6l8Kp1SU@projetocerp.1pv8p.mongodb.net" +
-		"/ProjetoCERP?retryWrites=true&w=majority"
-);
+db.setup("mongodb+srv://projetoCERP:Utw8ufjB6l8Kp1SU@projetocerp.1pv8p.mongodb.net" + "/ProjetoCERP?retryWrites=true&w=majority");
