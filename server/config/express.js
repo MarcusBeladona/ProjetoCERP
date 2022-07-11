009b7a465d90f374d153e880effafc82a9422bcc
Marcus Beladona <mv3265@gmail.com>
2022-07-10 21:19:39 -0300

    cors teste

--- server/config/express.js
+++ server/config/express.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const routes = require("./routes");
+var cors = require("cors");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 module.exports = app;
